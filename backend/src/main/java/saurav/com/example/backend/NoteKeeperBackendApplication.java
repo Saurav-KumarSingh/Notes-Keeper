9710341211c672d3a6f463aaaf76cb26419a7a35
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-12-12 20:13:05 +0530

    initial commit

---
+++ backend/src/main/java/saurav/com/example/backend/NoteKeeperBackendApplication.java
@@ -0,0 +1,13 @@
+package saurav.com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteKeeperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteKeeperBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/saurav/com/example/backend/NoteKeeperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package saurav.com.example.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteKeeperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
